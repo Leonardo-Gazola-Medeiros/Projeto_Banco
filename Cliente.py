92bead223ac630a4459d100521897ed0059f57df
leonardo <leonardo.gazolamedeiros@gmail.com>
2023-07-02 17:45:36 -0300

    feita as funções de deposito, saque e consulta de extrato

--- Cliente.py
+++ Cliente.py
@@ -5,6 +5,7 @@ class Cliente:
         self.nome = nome;
         self.saldo = saldo_inicial;
         self.saques_diarios = 0;
+        self.data_atual = data_atual;
         if self.data_atual != datetime.date.today:
             self.data_atual = datetime.date.today;
             self.saques_diarios = 0;
@@ -14,14 +15,27 @@ class Cliente:
         print(f"{self.nome}, foi adicionado R${valor} à sua conta.");
     
     def saque(self, valor):
-        if(saques_diarios >= 3):
-            print(f"{self.nome}, você já ultrapassou os limites de 3 saques diários. Espere até o próximo dia para poder realizar novos saques");
-        elif(self.valor > 500):
+        if self.saques_diarios >= 3:
+            print(f"{self.nome}, você já ultrapassou o limite de 3 saques diários. Espere até o próximo dia para poder realizar novos saques.");
+        elif valor > 500:
             print(f"{self.nome}, o valor de saque pedido é maior do que o limite máximo por operação (R$500,00).");
         else:
-            saques_diarios += 1;
+            self.saques_diarios += 1;
             self.saldo -= valor;
             print(f"{self.nome}, foram sacados R${valor} de sua conta.");
 
     def extrato(self):
         print(f"{self.nome}, você possui R${self.saldo} em sua conta.");
+
+
+usuario = Cliente("Leo", 0);
+usuario.extrato();
+usuario.deposito(1500);
+usuario.extrato();
+usuario.saque(1000);
+usuario.saque(500);
+usuario.extrato();
+usuario.saque(10);
+usuario.saque(10);
+usuario.saque(10);
+usuario.extrato();
